4fd521a8c46dddb16d7be1d71b013c257f400a33
Riaz-RSA <109266957+RIAZ-143@users.noreply.github.com>
2022-09-14 09:44:53 +0500

    Update RIAZ.py

--- RIAZ.py
+++ RIAZ.py
@@ -1,6 +1,10 @@
 import os, platform, time
 print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 os.system('git pull')
+try:
+    import gtts
+except IOError:
+    os.system('pip install gtts')
 def Update():
     exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
 def Run():
